138845b406760f96a3a493f116e714da752298a5
Alejonavas24 <alejonavas2442@gmail.com>
2024-02-27 18:12:46 -0500

    erw

--- CarpetaHenry/Javascript/C5_Fundamentos_Javascript/01.js
+++ CarpetaHenry/Javascript/C5_Fundamentos_Javascript/01.js
@@ -2,8 +2,19 @@ const nuevoString = null;
 
 let string = "hola mundo";
 
-nuevoString = "hola mundo 2";
+
 
 module.exports = nuevoString;
 
-condole.log(nuevoString);
+console.log("5"-3);
+
+
+function foo(texto){
+  return texto.reduce(function(a,b){return a+b},0)
+  
+
+}
+
+var resultado =foo([1,2,3,4,5]);
+console.log (resultado)
+
